70bc236f6d61a17535229cd4bf891e5d4a431874
aelpha <ola350@mail.ru>
2022-04-12 13:42:59 +0300

    Add properties

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -19,7 +19,7 @@ public class TestBase {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://" + System.getProperty("user") + ":" + System.getProperty("password") + "@"
+        Configuration.remote = "https://" + System.getProperty("login") + ":" + System.getProperty("password") + "@"
                 + System.getProperty("remoteBrowser");
 
         SelenideLogger.addListener("allure", new AllureSelenide());
